ee5a6920538247217b8746b9942c8abac97af44c
김성민 <127092314+minnnnnnnnnnn@users.noreply.github.com>
2025-01-02 15:14:33 +0900

    Create section1 chapter09.js

---
+++ section1 chapter09.js
@@ -0,0 +1,46 @@
+// 1. if 조건문 (if문)
+let num = 9;
+
+if(num >= 10){
+    console.log("num은 10 이상입니다.");
+}
+
+else if(num >= 5){
+    console.log("num은 5 이상입니다.");
+}
+
+else{
+    console.log("거짓입니다.");
+}
+
+// 2. switch 문
+// -> if문과 기능 자체는 동일
+// -> 다수의 조건을 처리할 때 if보다 더 직관적
+
+let animal = "cat";
+
+switch (animal){
+    case "cat":{
+        console.log("고양이"); 
+        break;
+    }
+    case" dog":{
+        console.log("강아지"); 
+        break;
+    }
+    case "bear":{
+        console.log("곰"); 
+        break;
+    }
+    case "snake":{
+        console.log("뱀"); 
+        break;
+    }
+    case"tiger":{
+        console.log("호랑이"); 
+        break;
+    }
+    default:{
+        console.log("해당 없음");
+    }
+}
